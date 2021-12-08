612b847dd1dd25cc50d1ef3976ab4473827ebb9b
Savan Dave <69035316+savan139@users.noreply.github.com>
2021-12-08 18:42:52 +0530

    Update stock_prediction.py

--- stock_prediction.py
+++ stock_prediction.py
@@ -1,7 +1,6 @@
-
+# pip install streamlit fbprophet yfinance plotly
 import streamlit as st
 from datetime import date
-
 import yfinance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
